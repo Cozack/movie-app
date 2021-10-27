e68855647f3a074f366215da0224ebeb1f11a3b7
Cozack <dudycholeg@gmail.com>
2021-10-27 21:34:31 +0300

    header added

---
+++ src/app/component/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  title = 'Best Movies App'
+  themeName = 'Black'
+  themeColor: any
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  themeSwitcher(target:any):void {
+    if (target.checked) {
+      document.body.classList.remove('black')
+      document.body.classList.add('white')
+      this.themeName = 'White'
+    }
+
+    else {
+      document.body.classList.remove('white')
+      document.body.classList.add('black')
+      this.themeName = 'Black'
+      this.themeColor = false
+    }
+  }
+}
